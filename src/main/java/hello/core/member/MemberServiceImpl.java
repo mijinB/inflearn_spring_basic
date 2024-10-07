71621762a567f09fb128aaad13922338f279c81b
Bmijin <bmj13465@jtts.co.kr>
2024-10-07 14:06:41 +0900

    회원 도메인 테스트

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,8 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
+    // 인터페이스(MemberRepository)를 의존하지만 구현 클래스(MemoryMemberRepository)도 의존하기 때문에 DIP 원칙 위반이다.
+    // 저장소를 변경하려면 아래 코드를 수정해야 하기 때문에 OCP 원칙도 위반한다.
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
