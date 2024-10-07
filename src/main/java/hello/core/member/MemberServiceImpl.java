83a0c86e503043687d2b94332b3738793bbe6677
mijinB <bmj13465@gmail.com>
2024-10-07 22:55:13 +0900

    관심사 분리하여 수정(AppConfig)

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -4,7 +4,14 @@ public class MemberServiceImpl implements MemberService {
 
     // 인터페이스(MemberRepository)를 의존하지만 구현 클래스(MemoryMemberRepository)도 의존하기 때문에 DIP 원칙 위반이다.
     // 저장소를 변경하려면 아래 코드를 수정해야 하기 때문에 OCP 원칙도 위반한다.
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // ↓ 인터페이스만 의존하고 있다. DIP 원칙 지키는 것. ⇒ 생성자 주입
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,17 +3,18 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    /* DIP(의존관계 역전) 원칙 위반
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); */
-    // 해결 ↓
-    private DiscountPolicy discountPolicy;
-    // ↳ NullPointerException 발생 ⇒ OrderServiceImpl에 DiscountPolicy의 구현 객체를 대신 생성하고 주입해주어야 해결된다.
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
