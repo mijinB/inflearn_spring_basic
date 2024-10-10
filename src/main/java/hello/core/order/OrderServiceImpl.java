03a9f7f0e195f21118c7c7e0d6fc25b3f716b097
Bmijin <bmj13465@jtts.co.kr>
2024-10-10 16:18:32 +0900

    조회 빈이 2개 이상일 경우 (문제 해결 코드)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -16,6 +16,18 @@ public class OrderServiceImpl implements OrderService {
         this.discountPolicy = discountPolicy;
     }
 
+    /*@Autowired 필드 명 매칭
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = rateDiscountPolicy;
+    }*/
+
+    /*@Qualifier @Qualifier끼리 매칭 빈 이름 매칭
+    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }*/
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
